1b26ffa4ec4a61b0b0c2d5846f64da4a1eb44a3a
N1ckl <145074533+N1ckl@users.noreply.github.com>
2024-04-02 23:42:05 -0400

    Update dice.hpp

--- dice.hpp
+++ dice.hpp
@@ -26,12 +26,15 @@ public:
 };
 class FakeDice:public CantStopDice {
 private:
-    ifstream inputFile;
+    ifstream inputfile;
 
 public:
     FakeDice();
+    roll();
     virtual ~FakeDice() override;
     virtual const int *roll() override;
+
 };
+
 inline ostream& operator << (ostream& output, Dice& d) {return d.print(output);}
 #endif
