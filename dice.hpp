b9e11769911d8c59b3e8a31abaec089e190d186d
N1ckl <145074533+N1ckl@users.noreply.github.com>
2024-04-08 01:02:46 -0400

    Update dice.hpp
    Roll(function isnt working)

--- dice.hpp
+++ dice.hpp
@@ -24,17 +24,18 @@ public:
     CantStopDice();
     virtual const int* newRoll();
 };
+
 class FakeDice:public CantStopDice {
 private:
-    ifstream inputfile;
+    ifstream inputFile;
 
 public:
     FakeDice();
     roll();
     virtual ~FakeDice() override;
     virtual const int *roll() override;
-
 };
 
 inline ostream& operator << (ostream& output, Dice& d) {return d.print(output);}
+
 #endif
