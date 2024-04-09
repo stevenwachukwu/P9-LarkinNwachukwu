892110ce6c4810b7786f2ab0981492b5dcc0c147
Steve Nwachukwu <144286017+stevenwachukwu@users.noreply.github.com>
2024-04-09 19:26:49 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include "dice.hpp"
-#include "Player.hpp"
+#include "player.hpp"
 #include "tools.hpp"
 #include "Enum.hpp"
 #include "column.hpp"
@@ -16,29 +16,25 @@ void unitCList();
 //-----------------------------------------------
 int main() {
     Game gaming;
-    //testGame(gameclass);
     gaming.start();
     //unitCList();
     bye();
     return 0;
 }
 //-----------------------------------------------
-void testGame(Game& games) {
-    games.unitTest();
-} //the unit test for game
 
 void unitCList() {
     ofstream CListNames ("CList.txt");
 
-upp test1("Jacob", ECcolor::blue);
-upp test2 ("Derek", ECcolor::green);
-upp test3 ("Olivia", ECcolor::orange);
-upp test4 ("Sadie", ECcolor::white);
+    upp test1 = make_unique <Player> (Player("Jacob", ECcolor::blue));
+    upp test2 = make_unique <Player> (Player("Derek", ECcolor::green));
+    upp test3 = make_unique <Player> (Player("Olivia", ECcolor::orange));
+    upp test4 = make_unique <Player> (Player("Sadie", ECcolor::white));
 
-Cell* person1 = new Cell (test1);
-Cell* person2 = new Cell (test2);
-Cell* person3 = new Cell (test3);
-Cell* person4 = new Cell (test4);
+Cell* person1 = new Cell (move (test1));
+Cell* person2 = new Cell (move (test2));
+Cell* person3 = new Cell (move (test3));
+Cell* person4 = new Cell (move (test4));
 
 CList obj;
 obj.addCell(person1);
@@ -52,35 +48,30 @@ obj.remove();
 obj.print(cout);
 
 obj.remove();
-obj.print(cout);
 
 obj.remove();
-obj.print(cout);
 
 obj.remove();
-obj.print(cout);
 
-obj.print(CListNames);
+upp test5 = make_unique <Player> (Player("Marcus", ECcolor::blue));
+upp test6 = make_unique <Player> (Player("Kelly", ECcolor::green));
+upp test7 = make_unique <Player> (Player("Maria", ECcolor::orange));
+upp test8 = make_unique <Player> (Player("Lucas", ECcolor::white));
 
-upp test5("Marcus", ECcolor::blue);
-upp test6 ("Kelly", ECcolor::green);
-upp test7 ("Maria", ECcolor::orange);
-upp test8 ("Lucas", ECcolor::white);
-
-Cell* person5 = new Cell (test5);
-Cell* person6 = new Cell (test6);
-Cell* person7 = new Cell (test7);
-Cell* person8 = new Cell (test8);
+Cell* person5 = new Cell (move (test5));
+Cell* person6 = new Cell (move (test6));
+Cell* person7 = new Cell (move (test7));
+Cell* person8 = new Cell (move (test8));
 
 obj.addCell(person5);
 obj.addCell(person6);
 obj.addCell(person7);
 obj.addCell(person8);
 
+obj.init();
 obj.next();
 obj.next();
 obj.next();
 obj.next();
-
 obj.print(CListNames);
 } //the unit test for CList
